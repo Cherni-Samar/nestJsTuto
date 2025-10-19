7c345baa1f1c7de737ac205a2cd58e2b09d5eec6
samar <chernisamar029@gmail.com>
2025-10-19 19:51:06 +0100

    options service crud

--- src/options/options.service/options.service.ts
+++ src/options/options.service/options.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { Option } from '../entities/option.entity';
+import { CreateOptionDto } from '../dto/create-option-dto';
 
 @Injectable()
-export class OptionsService{}
+export class OptionsService{
+
+    private options: Option[] = [];
+    private idCounter = 1;
+    create(createOptionDto: CreateOptionDto): Option {
+        const newOption: Option = {
+            id: this.idCounter++,
+            ...createOptionDto,
+        };
+        this.options.push(newOption);
+        return newOption;
+    }
+
+    findAll(): Option[] {
+        return this.options;
+    }
+    findOne(id:number): Option {
+        const option = this.options.find((u)=>u.id === id);
+        if (!option) {
+            throw new Error(`Option with ID ${id} not found`);
+        }
+        return option;
+    }
+
+    update(id: number, updateOptionDto: CreateOptionDto): Option {
+        const index = this.options.findIndex((u) => u.id === id);
+        if (index === -1) {
+            throw new Error(`Option with ID ${id} not found`);
+        }
+        const updatedOption: Option = {
+            id: id,
+            ...updateOptionDto,
+        } as Option;
+        this.options[index] = updatedOption;
+        return this.options[index];
+}
+}
